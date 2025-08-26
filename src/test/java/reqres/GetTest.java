aa356d1dc6701f504dfeb601681af6d592ad6a22
Sasi <sasikumarbala29@gmail.com>
2025-08-26 09:21:59 +0530

    added test

--- src/test/java/reqres/GetTest.java
+++ src/test/java/reqres/GetTest.java
@@ -1,34 +1,92 @@
 package reqres;
 
-
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
 public class GetTest {
 
-  @Test()
-   public void testGetUsers() { 
-      
-                given()
-                .when()
-                	.get("https://reqres.in/api/users")
-                .then()
-                	.statusCode(200)   
-                    .header("Content-Type", containsString("application/json"))
+    @Test
+    public void testGetUsers() {
+        given()
+            .baseUri("https://reqres.in")               // Base URI
+            .basePath("/api/users")                     // Base Path
+            .queryParam("page", 1)                      // Query parameter
+            .header("x-api-key", "reqres-free-v1")      // Custom API key header
+        .when()
+            .get()                                      // GET request
+        .then()
+            .statusCode(200)                            // Validate HTTP status
+            .header("Content-Type", containsString("application/json")) // Content-Type header
+            .header("Server", notNullValue())           // Server header
+            .header("Connection", equalTo("keep-alive")) // Connection header
 
-                	  .body("page", equalTo(1))  
-                      .body("per_page", equalTo(6)) 
-                      .body("data", notNullValue()) 
-                      .body("data.size()", greaterThan(0)) 
-                      .body("data[0].id", notNullValue())  
-                      .body("data[0].email", containsString("@reqres.in"))    
-                	.log().all();
-        
-    } 
-  
-   
+            // Body validations
+            .body("page", equalTo(1))
+            .body("per_page", greaterThan(0))
+            .body("data", notNullValue())
+            .body("data.size()", greaterThan(0))
+            .body("data[0].id", notNullValue())
+            .body("data[0].email", containsString("@reqres.in"))
+            .body("data[0].first_name", notNullValue())
+            .body("data[0].last_name", notNullValue()) 
+            
+        .log().all(); 
+    }
+    
+    @Test
+    public void testUserNotFound() {
+        given()
+            .baseUri("https://reqres.in")
+            .basePath("/api/users/23")  // Non-existent user
+            .header("x-api-key", "reqres-free-v1")
+        .when()
+            .get()
+        .then()
+            .statusCode(404)
+            .log().all();
+    }
+    
+    @Test
+    public void testAllEmails() {
+        given()
+            .baseUri("https://reqres.in")
+            .basePath("/api/users")
+            .queryParam("page", 1)
+            .header("x-api-key", "reqres-free-v1")
+        .when()
+            .get()
+        .then()
+            .statusCode(200)
+            .body("data.email", everyItem(containsString("@reqres.in")))
+            .log().all();
+    }
+    @Test
+    public void testResponseTime() {
+        given()
+            .baseUri("https://reqres.in")
+            .basePath("/api/users")
+            .queryParam("page", 1)
+            .header("x-api-key", "reqres-free-v1")
+        .when()
+            .get()
+        .then()
+            .statusCode(200)
+            .time(lessThan(2000L))  // Response time < 2 seconds
+            .log().all();
     }
 
-    
-    
+    @Test
+    public void testMissingApiKey() {
+        given()
+            .baseUri("https://reqres.in")
+            .basePath("/api/users")
+            .queryParam("page", 1)
+        .when()
+            .get()
+        .then()
+            .statusCode(401)  
+            .log().all();
+    }
+
+}
